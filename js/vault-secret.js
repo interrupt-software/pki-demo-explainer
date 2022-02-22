c9254fef93d96819168fa4bb8da28f4618431d1c
gcastill0 <gilberto@hashicorp.com>
2022-02-22 13:41:57 -0500

    Updated assets

---
+++ js/vault-secret.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+
+    const tale_teller = document.getElementById("tale-teller");
+    const copy_root_token = document.getElementById("copy-root-token");
+    const copy_icon = document.getElementById("copy-icon");
+
+    async function fetchRootToken() {
+        const response = await fetch("/js/vault-unseal.json");
+        const jsonbody = await response.json();
+        return jsonbody;
+    }
+
+    var root_token;
+
+    fetchRootToken().then(jsonbody => {
+        copy_root_token.innerHTML = jsonbody["root_token"];
+        root_token = jsonbody["root_token"];
+    });
+
+    tale_teller.addEventListener('click', () => {
+        navigator.clipboard.writeText(root_token);
+        copy_icon.innerHTML = `
+          <img src="/img/check-mark.svg" alt="Copied">
+        `
+    })
+
+}
