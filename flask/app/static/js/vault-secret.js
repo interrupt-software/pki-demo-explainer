376afd3742f523e0fcdb43a5156784afb62101db
gcastill0 <gilberto@hashicorp.com>
2022-09-29 13:34:20 -0400

    Updated copy command

--- flask/app/static/js/vault-secret.js
+++ flask/app/static/js/vault-secret.js
@@ -1,25 +1,25 @@
+async function fetchRootToken() {
+    const response = await fetch("/root_token");
+    const jsonbody = await response.json();
+    return jsonbody;
+}
+
 window.onload = function () {
 
     const tale_teller = document.getElementById("tale-teller");
     const copy_root_token = document.getElementById("copy-root-token");
     const copy_icon = document.getElementById("copy-icon");
 
-    async function fetchRootToken() {
-        const response = await fetch("/root_token");
-        const jsonbody = await response.json();
-        return jsonbody;
-    }
-
     var root_token;
 
     fetchRootToken().then(jsonbody => {
-        copy_root_token.innerHTML = jsonbody["root_token"];
-        root_token = jsonbody["root_token"];
+        copy_root_token.innerHTML = jsonbody["root_token"]
     });
 
     tale_teller.addEventListener('click', () => {
-        window.getSelection().selectAllChildren(copy_root_token);
-        navigator.clipboard.writeText(root_token);
-        copy_icon.innerHTML = `<img src="img/check-mark.svg" alt="Copied!">`;
+        window.getSelection().selectAllChildren(copy_root_token)
+        root_token = copy_root_token.innerText.trim()
+        document.execCommand("copy")
+        copy_icon.innerHTML = `<img src="img/check-mark.svg" alt="Copied!">`
     })
 }
